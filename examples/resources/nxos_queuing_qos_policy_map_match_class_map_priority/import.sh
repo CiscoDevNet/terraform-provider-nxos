4ea3089502e68377a225f65c58b766612819a5d8
danischm <danischm@cisco.com>
2022-03-26 11:38:36 +0100

    Add queuing qos policy map match class map priority resource

---
+++ examples/resources/nxos_queuing_qos_policy_map_match_class_map_priority/import.sh
@@ -0,0 +1,1 @@
+terraform import nxos_queuing_qos_policy_map_match_class_map_priority.example "sys/ipqos/queuing/p/name-[PM1]/cmap-[c-out-q1]/prio"
---
+++ examples/resources/nxos_queuing_qos_policy_map_match_class_map_priority/resource.tf
@@ -0,0 +1,5 @@
+resource "nxos_queuing_qos_policy_map_match_class_map_priority" "example" {
+  policy_map_name = "PM1"
+  class_map_name  = "c-out-q1"
+  level           = 1
+}

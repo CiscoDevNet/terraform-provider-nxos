01c11b54b4e1e474e459f906bd1aba6b6b950485
danischm <danischm@cisco.com>
2023-05-17 23:08:50 +0200

    Fix route map rule entry set regular community test

--- internal/provider/data_source_nxos_route_map_rule_entry_set_regular_community_test.go
+++ internal/provider/data_source_nxos_route_map_rule_entry_set_regular_community_test.go
@@ -35,11 +35,10 @@ resource "nxos_rest" "PreReq0" {
 }
 
 resource "nxos_rest" "PreReq1" {
-  dn = "sys/rpm/rtmap-[RULE1]/ent-[0]"
+  dn = "sys/rpm/rtmap-[RULE1]/ent-[10]"
   class_name = "rtmapEntry"
   content = {
-      name = "RULE1"
-      order = "0"
+      order = "10"
   }
   depends_on = [nxos_rest.PreReq0, ]
 }

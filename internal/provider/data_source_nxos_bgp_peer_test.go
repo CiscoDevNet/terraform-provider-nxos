e846bff6ce0806f62e3d59ec1ad2d70f8ddcafd5
Morten Skriver <moskrive@cisco.com>
2024-06-14 14:47:51 +0200

    add bfd as dependency for tests to pass

--- internal/provider/data_source_nxos_bgp_peer_test.go
+++ internal/provider/data_source_nxos_bgp_peer_test.go
@@ -60,28 +60,38 @@ resource "nxos_rest" "PreReq0" {
 }
 
 resource "nxos_rest" "PreReq1" {
-  dn = "sys/bgp"
-  class_name = "bgpEntity"
+  dn = "sys/fm/bfd"
+  class_name = "fmBfd"
+  delete = false
+  content = {
+      adminSt = "enabled"
+  }
   depends_on = [nxos_rest.PreReq0, ]
 }
 
 resource "nxos_rest" "PreReq2" {
+  dn = "sys/bgp"
+  class_name = "bgpEntity"
+  depends_on = [nxos_rest.PreReq1, ]
+}
+
+resource "nxos_rest" "PreReq3" {
   dn = "sys/bgp/inst"
   class_name = "bgpInst"
   content = {
       adminSt = "enabled"
       asn = "65001"
   }
-  depends_on = [nxos_rest.PreReq1, ]
+  depends_on = [nxos_rest.PreReq2, ]
 }
 
-resource "nxos_rest" "PreReq3" {
+resource "nxos_rest" "PreReq4" {
   dn = "sys/bgp/inst/dom-[default]"
   class_name = "bgpDom"
   content = {
       name = "default"
   }
-  depends_on = [nxos_rest.PreReq2, ]
+  depends_on = [nxos_rest.PreReq3, ]
 }
 
 `
@@ -102,7 +112,7 @@ resource "nxos_bgp_peer" "test" {
   ebgp_multihop_ttl = 5
   peer_control = "bfd,dis-conn-check"
   password = "secret_password"
-  depends_on = [nxos_rest.PreReq0, nxos_rest.PreReq1, nxos_rest.PreReq2, nxos_rest.PreReq3, ]
+  depends_on = [nxos_rest.PreReq0, nxos_rest.PreReq1, nxos_rest.PreReq2, nxos_rest.PreReq3, nxos_rest.PreReq4, ]
 }
 
 data "nxos_bgp_peer" "test" {

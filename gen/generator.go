54be5a689cff2a01c1cb2295a6c45d2ca7a5336e
danischm <danischm@cisco.com>
2026-02-20 13:06:00 +0100

    Simplify generator structs

--- gen/generator.go
+++ gen/generator.go
@@ -139,28 +139,16 @@ type YamlConfigAttribute struct {
 }
 
 type YamlConfigChildClass struct {
-	ClassName      string                      `yaml:"class_name"`
-	Rn             string                      `yaml:"rn"`
-	Type           string                      `yaml:"type"`
-	TfName         string                      `yaml:"tf_name"`
-	Description    string                      `yaml:"description"`
-	DocPath        string                      `yaml:"doc_path"`
-	Mandatory      bool                        `yaml:"mandatory"`
-	HideTf       bool                        `yaml:"hide_tf"`
-	Attributes   []YamlConfigAttribute       `yaml:"attributes"`
-	ChildClasses []YamlConfigChildChildClass `yaml:"child_classes"`
-}
-
-type YamlConfigChildChildClass struct {
-	ClassName  string                `yaml:"class_name"`
-	Rn         string                `yaml:"rn"`
-	Type       string                `yaml:"type"`
-	TfName     string                `yaml:"tf_name"`
-	Description string               `yaml:"description"`
-	DocPath    string                `yaml:"doc_path"`
-	Mandatory  bool                  `yaml:"mandatory"`
-	HideTf     bool                  `yaml:"hide_tf"`
-	Attributes []YamlConfigAttribute `yaml:"attributes"`
+	ClassName    string                 `yaml:"class_name"`
+	Rn           string                 `yaml:"rn"`
+	Type         string                 `yaml:"type"`
+	TfName       string                 `yaml:"tf_name"`
+	Description  string                 `yaml:"description"`
+	DocPath      string                 `yaml:"doc_path"`
+	Mandatory    bool                   `yaml:"mandatory"`
+	HideTf       bool                   `yaml:"hide_tf"`
+	Attributes   []YamlConfigAttribute  `yaml:"attributes"`
+	ChildClasses []YamlConfigChildClass `yaml:"child_classes"`
 }
 
 type YamlTest struct {

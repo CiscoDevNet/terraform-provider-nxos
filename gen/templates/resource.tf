c05ed245efc9f7825fdba5258a31c8a0189a934f
danischm <danischm@cisco.com>
2026-02-23 08:38:08 +0100

    Refactor resource example template

--- gen/templates/resource.tf
+++ gen/templates/resource.tf
@@ -1,62 +1,31 @@
+{{- /* renderChildren recursively renders TfChildClasses.
+       Context: map with "Children" ([]TfChildClass) and "Indent" (string). */ -}}
+{{- define "renderChildren" -}}
+{{- $indent := .Indent -}}
+{{- range .Children -}}
+{{- if eq .Type "single" -}}
+{{- range .Attributes}}
+{{$indent}}{{.TfName}} = {{if eq .Type "String"}}"{{end}}{{.Example}}{{if eq .Type "String"}}"{{end}}
+{{- end -}}
+{{- if .TfChildClasses -}}
+{{- template "renderChildren" (makeMap "Children" .TfChildClasses "Indent" $indent) -}}
+{{- end -}}
+{{- else if eq .Type "list"}}
+{{$indent}}{{.TfName}} = [{
+{{- range .Attributes}}
+{{$indent}}  {{.TfName}} = {{if eq .Type "String"}}"{{end}}{{.Example}}{{if eq .Type "String"}}"{{end}}
+{{- end -}}
+{{- if .TfChildClasses -}}
+{{- template "renderChildren" (makeMap "Children" .TfChildClasses "Indent" (printf "%s  " $indent)) -}}
+{{- end}}
+{{$indent}}}]
+{{- end -}}
+{{- end -}}
+{{- end -}}
+{{- /* ==================== end renderChildren ==================== */ -}}
 resource "nxos_{{snakeCase .Name}}" "example" {
 {{- range  .Attributes}}
   {{.TfName}} = {{if eq .Type "String"}}"{{end}}{{.Example}}{{if eq .Type "String"}}"{{end}}
-{{- end}}
-{{- range .TfChildClasses}}
-{{- if eq .Type "single"}}
-{{- range .Attributes}}
-  {{.TfName}} = {{if eq .Type "String"}}"{{end}}{{.Example}}{{if eq .Type "String"}}"{{end}}
-{{- end}}
-{{- range .TfChildClasses}}
-{{- if eq .Type "single"}}
-{{- range .Attributes}}
-  {{.TfName}} = {{if eq .Type "String"}}"{{end}}{{.Example}}{{if eq .Type "String"}}"{{end}}
-{{- end}}
-{{- range .TfChildClasses}}
-{{- if eq .Type "list"}}
-  {{.TfName}} = [{
-  {{- range .Attributes}}
-    {{.TfName}} = {{if eq .Type "String"}}"{{end}}{{.Example}}{{if eq .Type "String"}}"{{end}}
-  {{- end}}
-  }]
-{{- end}}
-{{- end}}
-{{- else if eq .Type "list"}}
-  {{.TfName}} = [{
-  {{- range .Attributes}}
-    {{.TfName}} = {{if eq .Type "String"}}"{{end}}{{.Example}}{{if eq .Type "String"}}"{{end}}
-  {{- end}}
-  }]
-{{- end}}
-{{- end}}
-{{- else if eq .Type "list"}}
-  {{.TfName}} = [{
-  {{- range .Attributes}}
-    {{.TfName}} = {{if eq .Type "String"}}"{{end}}{{.Example}}{{if eq .Type "String"}}"{{end}}
-  {{- end}}
-  {{- range .TfChildClasses}}
-  {{- if eq .Type "single"}}
-  {{- range .Attributes}}
-    {{.TfName}} = {{if eq .Type "String"}}"{{end}}{{.Example}}{{if eq .Type "String"}}"{{end}}
-  {{- end}}
-  {{- range .TfChildClasses}}
-  {{- if eq .Type "list"}}
-    {{.TfName}} = [{
-    {{- range .Attributes}}
-      {{.TfName}} = {{if eq .Type "String"}}"{{end}}{{.Example}}{{if eq .Type "String"}}"{{end}}
-    {{- end}}
-    }]
-  {{- end}}
-  {{- end}}
-  {{- else if eq .Type "list"}}
-    {{.TfName}} = [{
-    {{- range .Attributes}}
-      {{.TfName}} = {{if eq .Type "String"}}"{{end}}{{.Example}}{{if eq .Type "String"}}"{{end}}
-    {{- end}}
-    }]
-  {{- end}}
-  {{- end}}
-  }]
-{{- end}}
-{{- end}}
+{{- end -}}
+{{- template "renderChildren" (makeMap "Children" .TfChildClasses "Indent" "  ") }}
 }

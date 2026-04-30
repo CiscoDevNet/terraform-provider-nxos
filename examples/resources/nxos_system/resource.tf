38e4647388cf2d93dfd3cf097762bcc53a0bfbe1
danischm <danischm@cisco.com>
2026-04-30 21:53:33 +0200

    Add SSH server configuration to nxos_system resource and data source

--- examples/resources/nxos_system/resource.tf
+++ examples/resources/nxos_system/resource.tf
@@ -217,4 +217,19 @@ resource "nxos_system" "example" {
       }
     }
   }
+  ssh_admin_state                  = "enabled"
+  ssh_ciphers                      = "no"
+  ssh_description                  = "SSH access"
+  ssh_enable_weak_ciphers          = "no"
+  ssh_key_exchange_algorithms      = "no"
+  ssh_key_types                    = "no"
+  ssh_login_attempts               = 4
+  ssh_login_grace_time             = 60
+  ssh_message_authentication_codes = "no"
+  ssh_port                         = 22
+  keys = {
+    "rsa" = {
+      key_length = 2048
+    }
+  }
 }
